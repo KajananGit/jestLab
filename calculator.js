e9038f7f1a51d32ad00c0e3c3b470d0dd7a1fd27
Kajanan <Kajanan15i@gmail.com>
2024-03-12 13:36:15 +0000

    completed sum tasks

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const sum = function(a, b){
+    return a + b;
+};
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const modulus = (a, b) => a % b;
+
+const even = null;
+
+const odd = null;
+
+module.exports = { 
+    sum, 
+    subtract, 
+    multiply, 
+    divide, 
+    modulus, 
+    even, 
+    odd 
+};
---
+++ calculator.test.js
@@ -0,0 +1,61 @@
+const {
+  sum,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  even,
+  odd,
+} = require("./calculator");
+
+describe('sum', () => { 
+  
+  test('can add two small positive numbers', () => {
+    expected = 5;
+    actual = sum(2, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add two large positive numbers', () => { 
+    expected = 1500;
+    actual = sum(1000, 500);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add two negative numbers', () => {
+    expected = -5;
+    actual = sum(-2, -3);
+    expect(actual).toBe(expected);
+  });
+
+  test('can add zero', () => {
+    expected = 3;
+    actual = sum(0, 3);
+    expect(actual).toBe(expected);
+  });
+
+});
+
+describe('subtract', () => {
+
+});
+
+describe('multiply', () => {
+
+});
+
+describe('divide', () => {
+
+});
+
+describe('modulus', () => {
+
+});
+
+describe('even', () => {
+
+});
+
+describe('odd', () => {
+
+});
